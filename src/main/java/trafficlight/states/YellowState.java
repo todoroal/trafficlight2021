3fffac59e28f3dd53bf14ebb21cba75b3e36b6e3
todoroal <alexander.todorovic@stud.fh-campuswien.ac.at>
2021-04-11 17:03:26 +0200

    Create YellowState.java

---
+++ src/main/java/trafficlight/states/YellowState.java
@@ -0,0 +1,29 @@
+package trafficlight.states;
+
+import trafficlight.ctrl.TrafficLightCtrl;
+
+public class YellowState implements State{
+
+    private final TrafficLightCtrl ctrl;
+
+    public YellowState(TrafficLightCtrl ctrl) {
+        this.ctrl = ctrl;
+    }
+
+    @Override
+    public void nextState() {
+        if(ctrl.getPreviousState().equals(ctrl.getRedState())){
+            ctrl.setCurrentState(ctrl.getGreenState());
+            ctrl.setPreviousState(this);
+        }
+        else{
+            ctrl.setCurrentState(ctrl.getRedState());
+            ctrl.setPreviousState(this);
+        }
+    }
+
+    @Override
+    public TrafficLightColor getState() {
+        return TrafficLightColor.YELLOW;
+    }
+}

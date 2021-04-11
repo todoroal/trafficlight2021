9dfd74b32e2b334c7ebce9ed9464f84aba7a27fb
todoroal <alexander.todorovic@stud.fh-campuswien.ac.at>
2021-04-11 11:19:39 +0200

    Update MCP.java
    Implementierung Singleton

--- src/main/java/MCP.java
+++ src/main/java/MCP.java
@@ -3,7 +3,7 @@ import trafficlight.ctrl.TrafficLightCtrl;
 
 public class MCP {
     public static void main(String[] args) {
-        TrafficLightCtrl ctrl = new TrafficLightCtrl();
+        TrafficLightCtrl ctrl = TrafficLightCtrl.getInstance();
         ctrl.run();
     }
 }
